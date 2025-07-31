4c5707560a13c287b6c2b72ab77b0f294931274b
Torbjorn Tornkvist <ttornkvi@cisco.com>
2025-07-31 15:31:11 +0200

    Updating the root CA script

--- scripts/gen-root-ca.sh
+++ scripts/gen-root-ca.sh
@@ -7,11 +7,7 @@ else
     exit 1
 fi
 
-## generate rootca private key
-openssl genrsa  -out private/cakey.pem 4096
-
-## generate rootCA certificate
-openssl req -new -x509 -days 3650 -config openssl.cnf -key private/cakey.pem -out certs/cacert.pem -subj "/C=${CC}/ST=${STATE}/L=${CITY}/O=${ORG}/OU=root/CN=${CNAME}/emailAddress=${EMAIL}"
+openssl req -x509 -days 3650 -sha384 -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -keyout private/ca.key -nodes -subj "/C=${CC}/ST=${STATE}/L=${CITY}/O=${ORG}/OU=root/CN=${CNAME}/emailAddress=${EMAIL}" -out certs/ca.crt
 
 ## Verify the rootCA certificate content and X.509 extensions
-#openssl x509 -noout -text -in certs/cacert.pem
+#openssl x509 -noout -text -in certs/ca.crt
