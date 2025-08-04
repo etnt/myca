f48864f4ce71c951274c8fb9c47700967d814e9a
Torbjörn Törnkvist <kruskakli@gmail.com>
2025-08-04 10:27:16 +0200

    Ignore revoked check if no crl file and allow give CA cert as second argument

--- scripts/verify-crt.sh
+++ scripts/verify-crt.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env sh
+#
+# verify-crt.sh <Cert-File> [<CA-Cert-File>]
+#
+#   default CA-Cert-File is: cert/ca.crt
+#
 
 if [ ! -f "$1" ]; then
     echo "$0 : file not found: $1"
     exit 1
 fi
 
-openssl verify -CAfile certs/ca.crt -crl_check -CRLfile crl/rootca.crl "$1"
+CA_CERT="certs/ca.crt"
+if [ ! -z "$2" ]; then
+    CA_CERT="$2"
+fi
+
+
+if [ -f "./crl/rootcrl" ]; then
+    openssl verify -CAfile $CA_CERT -crl_check -CRLfile crl/rootca.crl "$1"
+else
+    echo "No ./crl/rootcrl file found, ignoring Revoked Check!"
+    openssl verify -CAfile  $CA_CERT "$1"
+fi
